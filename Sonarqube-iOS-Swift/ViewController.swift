590b36da10cabcdc2e9790b8bee9e9d9a987d99b
vunhoy.lim <vunhoy.lim@wingbank.com.kh>
2025-05-23 16:51:41 +0700

    Initial Commit

---
+++ Sonarqube-iOS-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sonarqube-iOS-Swift
+//
+//  Created by Vunhoy Lim on 23/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sonarqube-iOS-SwiftTests/Sonarqube_iOS_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sonarqube_iOS_SwiftTests.swift
+//  Sonarqube-iOS-SwiftTests
+//
+//  Created by Vunhoy Lim on 23/5/25.
+//
+
+import Testing
+@testable import Sonarqube_iOS_Swift
+
+struct Sonarqube_iOS_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
